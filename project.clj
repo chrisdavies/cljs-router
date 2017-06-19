0a9e272ab86c4ae7f1ea38a13de93f876b03aa74
Chris Davies <ferociousturtle@gmail.com>
2017-06-19 17:38:10 -0400

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-router "0.0.1-SNAPSHOT"
+(defproject cljs-router "0.0.1"
   :url "http://github.com/chrisdavies/cljs-router"
   :license {:name "MIT"}
   :description "A simple, order agnostic ClojureScript router"
